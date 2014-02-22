930284beb94ba4c00a78f65190690da5eba11193
Johny Urgiles <johny.urgiles@gmail.com>
2014-02-22 12:08:55 -0500

    Add a fading effect to screen

--- src/main/java/com/momus/SoundTent/runnables/MediaRecorderCaptor.java
+++ src/main/java/com/momus/SoundTent/runnables/MediaRecorderCaptor.java
@@ -10,22 +10,33 @@ import com.momus.SoundTent.activities.SoundTentActivity;
 
 public class MediaRecorderCaptor implements Runnable{
 
+    public static final double FADE_RATE = .9;
     private final MediaRecorder mediaRecorder;
     private final View view;
     private final Handler handler;
+    private Double maxAmplitude;
 
     @Inject
     public MediaRecorderCaptor(@Assisted MediaRecorder mediaRecorder, @Assisted View view, @Assisted Handler handler) {
         this.mediaRecorder = mediaRecorder;
         this.view = view;
         this.handler = handler;
+        this.maxAmplitude = Double.valueOf(Integer.MIN_VALUE);
     }
 
     @Override
     public void run(){
-        Integer maxAmplitude = mediaRecorder.getMaxAmplitude();
+        Double amplitude = Double.valueOf(mediaRecorder.getMaxAmplitude());
 
-        view.setBackgroundColor(Color.argb(255, maxAmplitude/98, 0, 0));
+        if(amplitude > maxAmplitude){
+            maxAmplitude = amplitude;
+        }else{
+            maxAmplitude = maxAmplitude * FADE_RATE;
+        }
+
+        int newColor = Color.rgb(maxAmplitude.intValue() / 98, 0, 0);
+
+        view.setBackgroundColor(newColor);
 
         handler.postDelayed(this, SoundTentActivity.DELAY_MILLIS);
     }
--- src/test/java/com/momus/SoundTent/runnables/MediaRecorderCaptorTest.java
+++ src/test/java/com/momus/SoundTent/runnables/MediaRecorderCaptorTest.java
@@ -21,6 +21,7 @@ import static org.robolectric.Robolectric.shadowOf;
 @RunWith(RobolectricTestRunner.class)
 public class MediaRecorderCaptorTest {
     public static final int AMP_VALUE = 1100;
+    public static final int HIGHER_AMP_VALUE = 5000;
     @Mock
     private Handler handler;
     @Mock
@@ -43,12 +44,27 @@ public class MediaRecorderCaptorTest {
     }
 
     @Test
-    public void shouldSetColorOfBackgroundToTextViewToMaxAmplitude() {
-        when(mediaRecorder.getMaxAmplitude()).thenReturn(AMP_VALUE);
+    public void shouldSetColorOfBackgroundRelativeToAmplitudeWhenHigherThanLastReading() {
+        when(mediaRecorder.getMaxAmplitude()).thenReturn(AMP_VALUE).thenReturn(HIGHER_AMP_VALUE);
 
-        new MediaRecorderCaptor(mediaRecorder, view, handler).run();
+        MediaRecorderCaptor mediaRecorderCaptor = new MediaRecorderCaptor(mediaRecorder, view, handler);
+        mediaRecorderCaptor.run();
+        mediaRecorderCaptor.run();
 
-        int expectedColor = Color.argb(255, AMP_VALUE/98, 0,0);
+        int expectedColor = Color.rgb(HIGHER_AMP_VALUE / 98, 0, 0);
+        assertThat(shadowOf(view).getBackgroundColor()).isEqualTo(expectedColor);
+    }
+
+    @Test
+    public void shouldSlowlyDarkenBackgroundColor() {
+        when(mediaRecorder.getMaxAmplitude()).thenReturn(HIGHER_AMP_VALUE).thenReturn(AMP_VALUE);
+
+        MediaRecorderCaptor mediaRecorderCaptor = new MediaRecorderCaptor(mediaRecorder, view, handler);
+        mediaRecorderCaptor.run();
+        mediaRecorderCaptor.run();
+
+        Double expectedAmplitude = HIGHER_AMP_VALUE * .9 / 98;
+        int expectedColor = Color.rgb(expectedAmplitude.intValue(), 0, 0);
         assertThat(shadowOf(view).getBackgroundColor()).isEqualTo(expectedColor);
     }
 }
